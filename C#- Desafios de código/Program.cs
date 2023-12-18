53cf000674bfda4edd077d222b48ffb765ff306c
Aline <113183673+Aline875@users.noreply.github.com>
2023-12-18 15:35:15 -0300

    Add files via upload

---
+++ C#- Desafios de código/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Desafio1 {
+    static void Main() 
+    {
+        Console.WriteLine("digite o titulo:");
+        string titulo = Console.ReadLine();
+
+        Console.WriteLine("digite a Descrição");
+        string descricao = Console.ReadLine();
+
+        Console.WriteLine("digite a data de vencimento;");
+        string dataVencimento = Console.ReadLine();
+
+        if(descricao.Length > 50)
+        {
+            Console.WriteLine("O números de caracteres excedidos na descrição!");
+        }
+        else{
+            Console.WriteLine("Tarefa Adicionada:");
+            Console.WriteLine($"Título: {titulo}");
+            Console.WriteLine($"Descrição: {descricao}");
+            Console.WriteLine($"Data de Vencimento: {dataVencimento}");
+        }
+    }
+}
