5007bd5b4f2a55381bc2d486f86220468aec5682
Aline <113183673+Aline875@users.noreply.github.com>
2023-12-18 16:57:56 -0300

    18-12

--- Desafio2/obj/Debug/net8.0/Desafio2.AssemblyInfo.cs
+++ Desafio2/obj/Debug/net8.0/Desafio2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53cf000674bfda4edd077d222b48ffb765ff306c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07750bb7c277f14512db5c708ed99e46482b098e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafio2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
