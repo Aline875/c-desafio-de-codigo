07750bb7c277f14512db5c708ed99e46482b098e
Aline <113183673+Aline875@users.noreply.github.com>
2023-12-18 16:49:57 -0300

    18-12

---
+++ Desafio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Desafio2
+{
+    static void Main()
+    {
+        int limiteInferior = int.Parse(Console.ReadLine());
+        int limiteSuperior = int.Parse(Console.ReadLine());
+
+        int somaPares = 0;
+
+        for (int i = limiteInferior; i <= limiteSuperior; i++)
+        {
+            if(i % 2 == 0)
+            {
+                somaPares += i;
+            }
+        }
+
+        Console.WriteLine($"A soma dos números pares de {limiteInferior} a {limiteSuperior} e: {somaPares}");
+
+    }
+}
